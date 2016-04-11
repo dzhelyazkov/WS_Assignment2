f2de0f5085e3c2ba6024e89078c4b43a0603b509
Arkadiy Shevrikuko <arkanoid555@gmail.com>
2016-04-11 01:27:05 +0100

    client final version
    added support of SOAP(with exception handling and debug parameter)

--- web_client/script.js
+++ web_client/script.js
@@ -1,9 +1,10 @@
-var dataObject = null;
+var dataObject = new Object();
 var errorObject = null;
 var method = null;
 var logged = 0;
 var registerFlag = 0;
 var statusString;
+var showMessages = 0;
 
 $(document).ready(function(){ 
 	checkUI();
@@ -11,7 +12,19 @@ $(document).ready(function(){
 		registerFlag = 1;
 		checkUI();
 	});
+	//testXML();
 });
+
+function testXML(){
+	var account = new Object();
+	account.id = -1;
+	account.name = "user1";
+	account.password = "user1";
+	console.log(createXMLMessage("register", account));
+	console.log(createXMLMessage("login", account));
+	console.log(createXMLMessage("save", account));
+	console.log(createXMLMessage("delete", account));
+}
 
 function processAction(type){
 	var account = new Object();
@@ -74,11 +87,11 @@ function checkUI(){
 
 function validateInputData(account){
 	if(account.name.match(/\n|,/)){
-		alert("Wrong username!");
+		alert("Wrong username's format!");
 		return -1;
 	}
 	if(account.password.match(/\n|,/)){
-		alert("Wrong password!");
+		alert("Wrong password's format!");
 		return -2;
 	}
 	return 0;
@@ -86,14 +99,16 @@ function validateInputData(account){
 
 function isFieldsEmpty(account){
 	if(account.name.length == 0 || account.password.length == 0){
-		alert("Epty fields");
+		alert("Empty fields");
 		return 1;
 	}
 	return 0;
 }
 
 function sendAJAXREST(operation, account){
-	var status = 0;
+	if(showMessages == 1){
+		console.log("REST request: " + JSON.stringify(account));
+	}
 	$.ajax({
 	  method: "POST",
 	  url: "http://localhost:8080/WS_Ass_try3_war_exploded/restService/rest/"+operation,
@@ -108,13 +123,17 @@ function sendAJAXREST(operation, account){
 			dataObject = msg;
 		}
 		errorObject = null;
-    	console.log("DONE");
-    	status = 1;
-    	processResponse(status,operation);
+    	//console.log("DONE");
+    	processResponse(1,operation);
   	}).fail(function( msg ){
   		dataObject = null;
   		alert(msg.responseJSON.error);
-  		processResponse(status,operation);
+  		processResponse(0,operation);
+  	}).always(function(msg){
+  		if(showMessages == 1){
+			console.log("REST response: ");
+			console.log(msg);
+		}
   	});
 }
 
@@ -143,9 +162,95 @@ function processResponse(status,operation){
 
 function sendAJAX(operation, account){
 	if(method == 'soap'){
-
+		sendAJAXSOAP(operation, account);
 	}
 	else{
-		sendAJAXREST(operation, account)
+		sendAJAXREST(operation, account);
+	}
+}  
+
+function sendAJAXSOAP(operation, account){
+	var xmlMes = createXMLMessage(operation, account);
+	if(showMessages == 1){
+		console.log("SOAP response: " + xmlMes);
+	}
+	$.ajax({
+	  method: "POST",
+	  url: "http://localhost:8080/WS_Ass_try3_war_exploded/AccountServiceService",
+	  contentType: "text/xml; charset=utf-8",
+	  dataType: "xml",
+	  data: xmlMes,
+	}).done(function( msg ) {
+    	//console.log(msg);
+    	parseResponse(msg, operation, account);
+    	processResponse(1,operation);
+  	}).fail(function( msg ){
+  		//console.log(msg);
+  		parseResponseError(msg.responseText);
+  		processResponse(0,operation);
+  	}).always(function(msg){
+  		if(showMessages == 1){
+			console.log("SOAP response: ");
+			console.log(msg);
+		}
+  	});;
+}
+
+function parseResponse(xml, operation, account){
+	if(xml != null && xml.nodeName != 'return'){
+		parseResponse(xml.firstChild,operation, account);
+	}
+	else{	
+		switch(operation){
+			case "save":
+				dataObject.name = account.name;
+				dataObject.password = account.password;
+			break;
+			case "delete":
+				dataObject = new Object();
+			break;
+			default:
+				dataObject.id = xml.childNodes[0].firstChild.data;
+				dataObject.name = xml.childNodes[1].firstChild.data;
+				dataObject.password = xml.childNodes[2].firstChild.data;
+			break;
+		}
+		//console.log(dataObject);
 	}
 }
+
+function parseResponseError(msg){
+	var arrBuf1 = msg.split("<faultstring>");
+	var error = arrBuf1[1].split("</faultstring>");
+	switch(error[0]){
+		case "model.exceptions.AccountDoesNotExistException":
+			alert("Account does not exist");
+		break;
+		case "model.exceptions.AccountNameExistsException":
+			alert("This username is taken");
+		break;
+		case "model.exceptions.AccountVerificationException":
+			alert("Wrong username or password format");
+		break;
+		case "model.exceptions.InvalidAccountParameterException":
+			alert("Wrong account parameter");
+		break;
+		default:
+			alert(error[0]);
+		break;
+	}
+}
+
+function createXMLMessage(operation, account){
+	var id = -1;
+	var name = "user1";
+	var password = "user1";
+
+	var start = "<?xml version='1.0' encoding='UTF-8'?><S:Envelope xmlns:S=\"http://schemas.xmlsoap.org/soap/envelope/\">"+
+				"<S:Body><ns2:"+operation+" xmlns:ns2=\"http://service/\">";
+
+	var args ="<arg0><id>"+account.id+"</id><name>"+account.name+"</name><password>"+account.password+"</password></arg0>";
+
+	var end = "</ns2:"+operation+"></S:Body></S:Envelope>";
+	return start + args + end;
+}  
